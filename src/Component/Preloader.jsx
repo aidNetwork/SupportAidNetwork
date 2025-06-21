a5432a8364023472e8d6951afda070f67f134ea5
aidNetwork <aidnetwork00@gmail.com>
2025-06-21 15:39:57 +0000

    feat: update Preloader component to change spinner color from yellow to blue

--- src/Component/Preloader.jsx
+++ src/Component/Preloader.jsx
@@ -4,7 +4,7 @@ const Preloader = () => {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-white z-50 space-y-4">
       {/* Spinner */}
-      <div className="w-16 h-16 border-4 border-yellow-500 border-t-transparent rounded-full animate-spin"></div>
+      <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
 
       {/* Pulsing Text */}
       <h2 className="text-xl font-semibold text-blue-500 animate-pulse">Loading...</h2>
